af7046c4cf17d1102ce4634448c14107f7a5aae8
Nicola Carboni <nicola@carboni.me>
2020-12-09 11:07:55 +0000

    Deployed ae1f628 with MkDocs version: 1.1.2

--- et/artworkCSV/artwork.sh
+++ et/artworkCSV/artwork.sh
@@ -12,8 +12,15 @@ rm -rf turtle.sh ;
 rm -rf ttl/ ;
 rm -rf *.csv ;
 
-unzip -q script.zip
-rm -rf __MACOSX
+cp ../scripts/prefixes.txt .
+cp ../scripts/csvcut.sh .
+cp ../scripts/mmd.sh .
+cp ../scripts/criteria.sh .
+cp ../scripts/turtle.sh .
+cp ../scripts/rename_png.sh .
+cp ../scripts/rename_csv.sh .
+cp -R ../scripts/src/ .
+
 
 python ../airscraper/airscraper/airscraper/airscraper.py https://airtable.com/shrPgJopfdzS8TRjU > Artwork_Name.csv ;
 python ../airscraper/airscraper/airscraper/airscraper.py https://airtable.com/shr1BUAc0k7CMGOPR > Artwork_Parthood.csv ;
